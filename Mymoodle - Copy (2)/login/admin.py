2e98034a49e08721eee96ae7412a9a3a35e58573
Araf020 <Araf020@users.noreply.github.com>
2020-11-25 02:19:11 +0600

    login

--- Mymoodle - Copy (2)/login/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-#from login.models import UserProfileInfo, CourseSession
-#admin.site.register(UserProfileInfo)
-#admin.site.register(CourseSession)
-# Register your models here.
